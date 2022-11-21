3787df2eda19939949d5388cd3e69fb5a525acb7
joannaberlin <joanna.kot.ok@gmail.com>
2022-11-21 12:08:01 +0100

    Set props in Product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,81 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = props => {
-  return (
-    <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
-      </div>
-      <div>
-        <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
-        </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
-      </div>
-    </article>
-  )
+const Product = (props) => {
+	const [currentColor, setCurrentColor] = useState(props.colors[0]);
+	const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+	console.log(props);
+	console.log(currentColor);
+	console.log(currentSize);
+
+	return (
+		<article className={styles.product}>
+			<div className={styles.imageContainer}>
+				<img
+					className={styles.image}
+					alt={props.title}
+					src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`}
+				/>
+			</div>
+			<div>
+				<header>
+					<h2 className={styles.name}>{props.title}</h2>
+					<span className={styles.price}>Price: {props.basePrice}$</span>
+				</header>
+				<form>
+					<div className={styles.sizes}>
+						<h3 className={styles.optionLabel}>Sizes</h3>
+						<ul className={styles.choices}>
+							<li>
+								<button type='button' className={styles.active}>
+									S
+								</button>
+							</li>
+							<li>
+								<button type='button'>M</button>
+							</li>
+							<li>
+								<button type='button'>L</button>
+							</li>
+							<li>
+								<button type='button'>XL</button>
+							</li>
+						</ul>
+					</div>
+					<div className={styles.colors}>
+						<h3 className={styles.optionLabel}>Colors</h3>
+						<ul className={styles.choices}>
+							<li>
+								<button
+									type='button'
+									className={clsx(styles.colorBlack, styles.active)}
+								/>
+							</li>
+							<li>
+								<button type='button' className={clsx(styles.colorRed)} />
+							</li>
+							<li>
+								<button type='button' className={clsx(styles.colorWhite)} />
+							</li>
+						</ul>
+					</div>
+					<Button className={styles.button}>
+						<span className='fa fa-shopping-cart' />
+					</Button>
+				</form>
+			</div>
+		</article>
+	);
+};
+
+Product.propTypes = {
+	title: PropTypes.string.isRequired,
+	basePrice: PropTypes.number.isRequired,
+	colors: PropTypes.array.isRequired,
+	sizes: PropTypes.array.isRequired,
 };
 
 export default Product;
